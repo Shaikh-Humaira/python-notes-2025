ab7017114c9ebee03490e55930a9607a72af1e53
Shaikh Humaira <shaikhhumaira110@gmail.com>
2026-06-03 23:03:56 +0530

    Refactor conditional statements and examples
    Updated comments and formatting for clarity and consistency.

--- 03_conditional_statements.py
+++ 03_conditional_statements.py
@@ -1,8 +1,10 @@
 """Conditional Statements"""
+# Conditional statements are used to control the flow of code
+# based on whether a condition is True or False
 
 # if-elif-else (SYNTAX)
 
-age = int(input("enter your age:"))
+age = int(input("Enter your age:"))
 
 if(age >= 18 and age < 60):
     category = "adult" 
@@ -15,25 +17,24 @@ elif(age >= 60):
 else:
     category = "dead/unborn"
 print("The person is:", category )
+#note
+# if checks the first condition, if it is False, Python checks elif condition(s).
+# elif can only be used after an if.
+# You can have multiple elif statements.
+# If all conditions are False, the else block executes.
+# else is optional and can be written only once.
+# Python uses indentation (4 spaces or a tab) to define code blocks.
 
-#NOTES
-#a. If if's statement isn't true then we check elif-if/ 2nd if statement(to write elif statement, if statement is mandatory)
-#b. You can write as many if/elif statements as needed
-#c. If all if/elif statements, the else statement will execute, its can be written once only
-#d. Python uses indentation(tab/4 spcaes)
+#Excersize
 
-
-
-#Examples
-
-# 1. check if a num entered by the user is even or odd 
+# Q1. check if a num entered by the user is even or odd 
 num = int(input ("enter the number:"))
 if (num % 2 == 0): #or we can write rem=num % 2 and if (rem == 0)
     print ("EVEN")
 else:
     print ("ODD")
  
-# 2. find the greatest of 3 num entered by the user
+# Q2. find the greatest of 3 num entered by the user
 a = int(input ("num A:"))
 b = int(input ("num B:")) 
 c = int(input ("num C:")) 
@@ -44,8 +45,8 @@ elif(b > c and b > a):
 else:
     print("c is the greatest number")
 
-# 3. find if the num is multiple of 7
-numb = int(input("enter the number:"))
+# Q3. find if the num is multiple of 7
+num = int(input("enter the number:"))
 if(num*7):
     print("Yes, the number is multiple of seven")
 else:
